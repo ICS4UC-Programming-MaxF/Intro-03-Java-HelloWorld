09227a9a2cd961811e7565d57a79f1391f67a6a2
Max fairman <Maximiliano.fairman@stu.ocsb.ca>
2026-09-04 13:23:11 +0000

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+* when run makes computer say hello world
+* @author  Maximiliano Fairman
+* @version 1.0
+* @since   2026-9-04
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
